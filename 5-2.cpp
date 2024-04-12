5b18fdb03755103a3c2b5b90fd399f0aa0c0b7ca
Jacob Lopez <jacob.lopez.2042@gmail.com>
2024-04-12 17:15:25 -0600

    first commit

---
+++ 5-2.cpp
@@ -0,0 +1,28 @@
+//Jacob Lopez
+//4-6-2024
+//5-2 Assignment
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main() {
+  ifstream inFS; //input file stream
+  ofstream outFS; //output file stream
+  string city;
+  int tempFarenheit;
+  double tempCelsius;
+  inFS.open("FahrenheitTemperature.txt");
+  if (!inFS.is_open()) {
+    cout << "Could not open FahrenheitTemperature.txt." << endl;
+    return 1;
+  }
+  while(inFS >> city >> tempFarenheit) {
+    tempCelsius = (tempFarenheit - 32) * ((double) 5 / (double) 9);
+    outFS << city << " " << tempCelsius << endl;
+  }
+
+  inFS.close();
+  outFS.close();
+  return 0;
+}
